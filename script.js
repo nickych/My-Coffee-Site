05d99be97bae47918cc7ffbe0c08630b8006e1c4
NICKY CHIBILIKA <53586666+nickych@users.noreply.github.com>
2024-01-21 15:42:35 -0800

    Update script.js

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ document.querySelector('#search-btn').onclick = () =>{
     cartItem.classList.remove('active');
 }
 
+
 let cartItem = document.querySelector('.cart-items-container');
 
 document.querySelector('#cart-btn').onclick = () =>{
